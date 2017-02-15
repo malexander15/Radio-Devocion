124bb41e4f2b3a29162441c08c8e051fada55c24
malexander15 <creepinjezus15>
2017-02-15 18:29:41 -0500

    set up authent for users from scratch

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,24 @@
 Rails.application.routes.draw do
   
+get 'signup', to: 'users#new', as: 'signup'
 
-  get 'sessions/new'
+get 'login', to: 'sessions#new', as: 'login'
+
+get 'logout', to: 'sessions#destroy', as: 'logout'
+
+
+get 'static_pages/about'
+
+get 'static_pages/home'
+
+get 'sessions/new'
 
   resources :users
   resources :sessions
   resources :casts
-  get 'static_pages/about'
 
-  get 'static_pages/home'
 
-  root 'static_pages#home'
+
+
+root 'static_pages#home'
 end
