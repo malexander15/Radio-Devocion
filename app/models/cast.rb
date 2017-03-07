b15323c6ce885a5d3be16e1ff804035847ac224a
malexander15 <creepinjezus15>
2017-03-07 18:47:58 -0500

    Had some typos/erraneous syntax that got saved and messed with some styles

--- app/models/cast.rb
+++ app/models/cast.rb
@@ -1,4 +1,6 @@
 class Cast < ApplicationRecord
+  attr_accessor :cast_thumbnail
+
   has_attached_file :cast_thumbnail, :styles => { :large => "1000x1000#", :medium => "550x550#" }
   validates_attachment_content_type :cast_thumbnail, :content_type => /\Aimage\/.*\Z/
 
